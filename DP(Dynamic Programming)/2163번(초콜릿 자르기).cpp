9b06db6c5cfc6412b4377980cea03d9325090bd4
bae haram <gkfkagkfka12@naver.com>
2018-06-27 16:17:28 +0900

    Update to DP solution

--- DP(Dynamic Programming)/2163번(초콜릿 자르기).cpp
+++ DP(Dynamic Programming)/2163번(초콜릿 자르기).cpp
@@ -1,8 +1,18 @@
 #include <cstdio>
 
+int dp[301][301];
+
 int main(void)
 {
 	int n, m; scanf("%d%d", &n, &m);
-	printf("%d", n*m - 1);
+	dp[1][0] = -1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (i>=2 && j == 1) dp[i][j] = dp[i - 1][j] + 1;
+			else dp[i][j] = dp[i][j - 1] + dp[i][1] + 1;
+			printf("%d x %d = %d°¡Áö\n",i,j,dp[i][j]);
+		}
+	}
+	printf("%d", dp[n][m]);
 	return 0;
 }
